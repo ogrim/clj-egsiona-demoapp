4464837daadcdd4e2020ee2ca90b252062c935cf
Aleksander Skjæveland Larsen <aleksander@ogrim.no>
2012-03-13 18:44:15 +0100

    Working on demoapp

--- project.clj
+++ project.clj
@@ -5,8 +5,9 @@
                  [net.cgrand/moustache "1.1.0"]
                  [enlive "1.0.0"]
 ;                 [google-maps "0.5.1-SNAPSHOT"]
-                 [clj-egsiona "0.1.0-SNAPSHOT"]
-                                        ;                 [sqlitejdbc "0.5.6"]
-                                        ;[org.xerial/sqlite-jdbc "3.6.13"]
+                 [clj-egsiona "0.1.1"]
                  [org.xerial/sqlite-jdbc "3.7.2"]
+                 [de.l3s.boilerpipe/boilerpipe "1.1.0"]
+                 [xerces "2.4.0"]
+                 [net.sourceforge.nekohtml/nekohtml "1.9.15"]
                  ])
--- src/demoapp/core.clj
+++ src/demoapp/core.clj
@@ -4,7 +4,47 @@
         [ring.middleware resource reload file]
         [ring.util.response]
         [net.cgrand.moustache])
-  (:require [clj-egsiona.core :as e]))
+  (:require [clj-egsiona.core :as e])
+  (:import [java.net URL]
+           [de.l3s.boilerpipe.extractors ArticleExtractor DefaultExtractor]
+           ))
+
+(defn in? [element seq]
+  (if (some #{element} seq) true false))
+
+(def whitelisted-letters
+  #{\A \B \C \D \E \F \G \H \I \J \K \L \M \N \O \P \Q \R \S \T \U \V \W \X \Y \Z
+    \a \b \c \d \e \f \g \h \i \j \k \l \m \n \o \p \q \r \s \t \u \v \w \x \y \z
+    \ª \µ \º \À \Á \Â \Ã \Ä \Å \Æ \Ç \È \É \Ê \Ë \Ì \Í \Î \Ï \Ð \Ñ \Ò \Ó \Ô \Õ \Ö
+    \Ø \Ù \Ú \Û \Ü \Ý \Þ \ß \à \á \â \ã \ä \å \æ \ç \è \é \ê \ë \ì \í \î \ï \ð \ñ
+    \ò \ó \ô \õ \ö \ø \ù \ú \û \ü \ý \þ \ÿ \space \0 \1 \2 \3 \4 \5 \6 \7 \8 \9 \.
+    \: \newline \! \? \= \- \, \{ \} \[ \] \( \) \_ \\ \/ \; \' \@ \# \$ \% \& \*
+    \| })
+
+(defn stop-chars [url]
+  (distinct (filter #(not (in? % whitelisted-letters)) (extract-content url))))
+
+(defn clean-text [s]
+  (->> (filter #(in? % whitelisted-letters) s)
+       reverse
+       (drop-while #(= % \newline))
+       reverse
+       (apply str)))
+
+(defn extract-content [url]
+  (.. DefaultExtractor INSTANCE (getText (URL. url))))
+
+(comment (defn extract-content [url]
+   (.. ArticleExtractor INSTANCE (getText (URL. url)))))
+
+(defn process-url [url]
+  (-> url
+      extract-content
+      clean-text
+      e/process-text))
+
+(def p1 "http://www.bt.no/nyheter/innenriks/Spar-sterk-innvandrervekst-i-byene-2669852.html")
+(def p2 "http://www.bt.no/nyheter/lokalt/Venter-pa-godkjenning-2669298.html")
 
 (def api-key "AIzaSyA0IrRqPrqcuQTdbUS5o57-EsPEbFKRsOc")
 
@@ -14,29 +54,20 @@
 (def routes
   (app
    [""] page
+   ["test"] {:get page}
    ["map" &] (delegate map-page)))
 
-(defonce server (run-jetty #'routes {:port 8080 :join? false}))
+(defonce server (run-jetty #'routes {:port 8081 :join? false}))
 
-(e/set-obt "/home/ogrim/bin/The-Oslo-Bergen-Tagger")
+(e/set-obt "localhost:8085")
+;(e/set-obt "/home/ogrim/bin/The-Oslo-Bergen-Tagger")
 
 (comment (e/process-text "Tror du at Sandnes er en lokasjon?"))
-
-(comment (e/set-db {:classname "org.postgresql.Driver"
-            :subprotocol "postgresql"
-            :subname  "//localhost:5432/ogrimtest"
-            :user "postgres"
-            :password "babbafet"}))
-
-
 
 (e/set-db {:classname   "org.sqlite.JDBC"
            :subprotocol "sqlite"
            :subname     "database.db"})
 
-(set-db {:classname   "org.sqlite.JDBC"
-         :subprotocol "sqlite"
-         :subname     "database.db"})
 
 ;(e/create-tables)
 ;(e/process-text "Tror du at Sandnes er en lokasjon?")
